2fda15faf100b904c06ea1acbee939a76000f418
David Horne <dijit27@gmail.com>
2012-11-07 19:52:10 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Micropostalyptic::Application.config.secret_token = 'd6783a02a0fd8de6cf2d5519f4281fa293fd94b9aa226a6bf2fb724d8835be485abe30e409d45047db13526507ff697e54efc6d239ddf777936491df0f2baf24'
